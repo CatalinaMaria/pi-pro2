26335ba9204702cfec746c9433093e6cd79205b9
cosycka <cataosycka@live.com.ar>
2023-04-05 14:43:30 -0300

    cambie los nombres de unas imagenes y cree la base de un comentario

--- data/data.js
+++ data/data.js
@@ -1,81 +1,83 @@
 const data = {
     usuarios: {
-        email: 'cata@gmail.com',
-        nombre:'Cata ',
+        email: 'c@gmail.com',
+        nombre: 'Cata ',
         contrasenia: 'contrasenia',
         fechanacimiento: '21/02/2005',
         dni: '4666666',
-        fotoperfil: '/images/users/fotoperfil1.jpg' ,
+        fotoperfil: '/images/users/fotoperfil1.jpg',
     },
     productos: [
         {
-            imagen: 'images/products/acotar.webp',
+            imagen: '/images/products/acotar.webp',
             nombre: 'A court of thorns and roses',
             descripcion: 'First book of the ACOTAR series written by Sarah J Maas.',
-            fecha: 12 / 01 / 2023,
+            fecha: '12 / 01 / 2023',
         },
         {
-            imagen: 'images/products/caraval.jpg',
+            imagen: '/images/products/caraval.jpg',
             nombre: 'Caraval',
             descripcion: 'First book of the Caraval trilogy. It is just a game... isnt it?',
-            fecha: 4 / 02 / 2023,
+            fecha: '4 / 02 / 2023',
         },
         {
-            imagen: 'images/products/city of bones.webp',
+            imagen: '/images/products/cityofbones.webp',
             nombre: 'City of bones',
             descripcion: 'Part of the Shadow Hunter series written by Cassandra Clare',
-            fecha: 20 / 03 / 2023,
+            fecha: '20 / 03 / 2023',
         },
         {
-            imagen: 'images/products/divergent.webp',
+            imagen: '/images/products/divergent.webp',
             nombre: 'Divergent',
             descripcion: 'First book of the divergent trilogy',
-            fecha: 1 / 04 / 2023,
+            fecha: '1 / 04 / 2023',
         },
         {
-            imagen: 'images/products/house of earth and blood.webp',
+            imagen: '/images/products/houseofearthandblood.webp',
             nombre: 'House of Earth and blood',
             descripcion: 'The first Crescent City novel. Meet Bryce Quinland and Hunt Atalar',
-            fecha: 12 / 01 / 2023,
+            fecha: '12 / 01 / 2023',
         },
         {
-            imagen: 'images/products/little women.webp',
+            imagen: '/images/products/littlewomen.webp',
             nombre: 'Little Women',
             descripcion: 'The classic has come back in the best edition ever made.',
             fecha: 4 / 02 / 2023,
         },
         {
-            imagen: 'images/products/pillars of the earth.webp',
+            imagen: '/images/products/pillarsoftheearth.webp',
             nombre: 'Pillars of the Earth',
             descripcion: 'First book of the best fantasy world ever created',
-            fecha: 20 / 03 / 2023,
+            fecha: '20 / 03 / 2023',
         },
         {
-            imagen: 'images/products/shadow and bone.webp',
+            imagen: '/images/products/shadowandbone.webp',
             nombre: 'Shadow and Bone',
             descripcion: 'Now adapted to screen by netflix. Follow Alina as she discovers the perks of being a sun summoner',
-            fecha: 1 / 04 / 2023,
+            fecha: '1 / 04 / 2023',
         },
         {
-            imagen: 'images/products/six of crows 1.webp',
+            imagen: '/images/products/sixofcrows1.webp',
             nombre: 'Six of crows',
             descripcion: 'Kaz Brekker summons a team of thieves for a multimillion dollar quest',
-            fecha: 12 / 01 / 2023,
+            fecha: '12 / 01 / 2023',
         },
         {
-            imagen: 'images/products/the handsmaids tale.webp',
+            imagen: '/images/products/thehandsmaidstale.webp',
             nombre: 'The hand maids tale',
             descripcion: 'A classic of modern literature',
-            fecha: 4 / 02 / 2023,
+            fecha: '4 / 02 / 2023',
         },
     ],
     comentarios: [
         {
+            nombreusuario: '',
+            textocom: '',
+            fotoperfil: '/images/users/default-image.png',
+        },
+        {
 
-    },
-    {
-
-    }],
+        }],
 }
 
 module.exports = data;
