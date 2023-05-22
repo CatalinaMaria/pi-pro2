c8abfd06d12b891a2f62749f267ef6d2e1ab6d67
MariaRiobo <mrioboarrizabalaga@udesa.edu.ar>
2023-05-22 11:51:44 -0300

    Revision

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/indexController');
 
-router.get('/index', indexController.index);
+router.get('/', indexController.index);
 router.get('/search-results', indexController.searchresults);
 
 module.exports = router;
