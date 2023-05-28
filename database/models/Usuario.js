59e7c2f2f6de187996fabac3a631b400d6bfb8a2
MariaRiobo <mrioboarrizabalaga@udesa.edu.ar>
2023-05-28 13:41:59 -0300

    Modelo usuario

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -1,1 +1,42 @@
-module.exports =
+module.exports = function (sequelize, dataTypes) {
+    let alias = "Usuario";
+    let cols = {
+        id: {
+            autoIncrement : true,
+            primaryKey : true,
+            type: dataTypes.INTEGER
+        },
+        email: {
+            type: dataTypes.STRING(400)
+        },
+        contraseña: {
+            type: dataTypes.STRING(400)
+        },
+        fotoPerfil: {
+            type: dataTypes.STRING(400)
+        },
+        fecha: {
+            type: dataTypes.DATE
+        },
+        dni: {
+            type: dataTypes.INTEGER
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt: {
+            type: dataTypes.DATE
+        },
+        deletedAt: {
+            type: dataTypes.DATE
+        },
+
+    }
+    let config = {
+        tableName: "usuarios",
+        timestamps: false,
+        underscored: true
+    }
+    let Usuario = sequelize.define (alias, cols, config);
+    return Usuario;
+}
