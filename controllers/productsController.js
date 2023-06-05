63dd861a584abf19703c44e05f54d8c57f52e81d
CatalinaMaria <cdearzubiagamenghi@udesa.edu.ar>
2023-06-05 10:06:41 -0300

    error de nodemon

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,14 @@
 const data= require('../data/data');
 const db = require ("../database/models");
+const productsController = {
+  product: function (req, res){
+      let id = req.params.id;
+      res.render('product', {products: data.productos[id], comentarios: data.comentarios});
+  },
+  productadd: function(req,res){
+      res.render('product-add', {usuarios: data.usuarios});
+  },
+};
 
   db.Product.findAll({
     include: [
@@ -7,15 +16,7 @@ const db = require ("../database/models");
       {associate: "productoComentarios"}
     ]
   }).then(resultados=>{
-    const productsController = {
-      product: function (req, res){
-          let id = req.params.id;
-          res.render('product', {products: data.productos[id], comentarios: data.comentarios});
-      },
-      productadd: function(req,res){
-          res.render('product-add', {usuarios: data.usuarios});
-      },
-  };
+    let resultado = productsController
   })
 
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,26 +1,28 @@
 const data= require('../data/data');
 const db = require ("../database/models");
+
+const usersController = {
+  register: function(req,res){
+      res.render('register')
+  },
+  login: function (req,res) {
+      res.render('login')
+  },
+  profile: function (req, res){
+      res.render('profile' , {usuarios: data.usuarios, products: data.productos, comentarios: data.comentarios})
+  },
+  edit: function (req,res){
+      res.render('profile-edit', {usuarios: data.usuarios})
+  },
+};
+
 db.Usuario.findAll({
     include: [
       {associate: "productoUsuario"},
       {associate: "comentarioUsuario"}
     ]
   }).then(resultados=>{
-    const usersController = {
-      register: function(req,res){
-          res.render('register')
-      },
-      login: function (req,res) {
-          res.render('login')
-      },
-      profile: function (req, res){
-          res.render('profile' , {usuarios: data.usuarios, products: data.productos, comentarios: data.comentarios})
-      },
-      edit: function (req,res){
-          res.render('profile-edit', {usuarios: data.usuarios})
-      },
-  };
-  
+    let resultado = usersController
   })
   
 module.exports = usersController;
