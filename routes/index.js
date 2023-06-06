668e6f4a9f98a8617570d6ede08b0326a9374096
MariaRiobo <mrioboarrizabalaga@udesa.edu.ar>
2023-06-06 08:50:07 -0300

    Post

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.index);
 router.get('/search-results', indexController.searchresults);
-
+router.get('/search-results/create', indexController.create);
+router.post('/search-results/store', indexController.store);
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,11 @@ router.get('/register', usersController.register);
 router.get('/login', usersController.login);
 router.get('/myprofile', usersController.profile);
 router.get('/edit', usersController.edit);
+router.get('/register/create', usersController.create);
+router.post('/register/store', usersController.store);
+router.get('/login/create', usersController.create);
+router.post('/login/store', usersController.store);
+router.get('/edit/create', usersController.create);
+router.post('/edit/store', usersController.store);
 
 module.exports = router;
