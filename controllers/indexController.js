a53775721234907eda475c965c7067d1f73b71c3
CatalinaMaria <cdearzubiagamenghi@udesa.edu.ar>
2023-06-12 12:50:40 -0300

    search-results

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 const db = require ("../database/models");
 const usuario = db.Usuario;
 const producto =db.Product;
-const op = db.Sequelize.op;
+const op = db.Sequelize.Op;
 
 
 const indexController = {
@@ -12,35 +12,70 @@ const indexController = {
       {association: "comentarioUsuario"}
     ]
       }
-    producto.findAll({
-      order: [['idProducto', 'ASC']],
-      limit: 12,
-    }).then(function(data){
-      
-  res.render('index', {products: data})})
-        
-      },
-    searchresults: function (req, res){
-      let search = req.query.search;
-      console.log(search)
-      producto.findAll({
-        where: {
-          [op.or] : [
-            {producto: {[op.like]: `%${search}%`}},
-            {desripcionProducto: {[op.like]: `%${search}%`}},
-          ]
-        },
-        order: ['createdAt', 'DESC'],
-        include: [
-          {association: "productoUsuario"},],
-      })
-      .then(products=> {
-        if (products){
-          res.render ('search-results', (products, search))
-        }
-      } )
-          
     },
+
+    results: function (req, res){
+      let busqueda = req.query.busqueda;
+      console.log(busqueda);
+      let relaciones ={
+          include: [
+            {association: "productoUsuario"},
+            {association: "comentarioUsuario"}
+          ]
+      };
+      let criterio = {
+          where:{
+              [op.or]:[
+                  {nombre: {[op.like]:'%'+busqueda+'%'}},
+                  {descripcion:{[op.like]:'%'+busqueda+'%'}}
+              ]
+          },
+          order: [['createdAt', 'DESC']]
+      }
+      Producto.findAll(criterio, relaciones)
+          .then(function(data){
+              //return res.send(data);
+              return res.render('search-results', {products: data});
+          })
+          .catch(function(error){
+              console.log(error);
+         
+      })    
+      
+  },
+  searchresultsUser: function(req,res) {
+      let busqueda = req.query.search;
+      let criterio = {
+          where: {
+              [op.or]: [{
+                    nombre: {
+                       [op.like]: "%"+busqueda+"%"
+                    }
+                 },
+                 {
+                    email: {
+                       [op.like]: "%"+busqueda+"%"
+                    }
+                 }
+              ]
+           }
+          
+          }
+          Usuario.findAll(criterio)
+
+          .then(function(user){
+
+              
+
+              return res.render('searchresults-user', {user : user})
+                          
+                      
+                       
+          }).catch(function(err) {
+              console.log(err);
+          })    
+      },
+    
     store: function (req,res){
       console.log(req.body);
       res.redirect("/search-results")
